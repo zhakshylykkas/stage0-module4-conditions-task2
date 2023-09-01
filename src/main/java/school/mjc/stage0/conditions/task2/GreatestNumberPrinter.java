b9e575ba4a329fb575fc7aa9d9bd9fa1caca1a13
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-02 02:44:47 +0600

    Boolean conditions tasks commit

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first >= second) {
+            System.out.println(first);
+        } else {
+            System.out.println(second);
+        }
     }
 }
